ca7df0d076e7ec80c029484447dedb82c52dfb2f
isaaregina <isabellaregina344@gmail.com>
2023-12-29 21:50:22 -0300

    feat: adicionado prime vue

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import './style.css';
+import App from './App.vue';
 
-createApp(App).mount('#app')
+import { createApp } from 'vue';
+import PrimeVue from 'primevue/config';
+
+createApp(App).app.use(PrimeVue).mount('#app');
